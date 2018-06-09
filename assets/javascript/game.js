bdb489d07769ff7ff2ea1e5094bfdb2d8261336b
shellyrgarner <sgrenea@gmail.com>
2018-06-09 01:31:21 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,36 @@
 var crystals = $("#crystals");
 
 var counter = 0;
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var randNum;
-var numberOptions = [3, 5, 7, 10];
+var randCrysNum;
+var imageCrystal;
 
+function startGame() {
+
+    crystals.empty();
 //calculates random number to guess
-var randNum = Math.floor(Math.random() * 101) + 19;
+randNum = Math.floor(Math.random() * 101) + 19;
 $("#number-to-guess").text(randNum);
-
 
 //calculates random numbers for crystals
 for (var i = 0; i < 4; i++) {
 
-    var randCrysNum = Math.floor(Math.random() * 11) + 1;
-    console.log(randCrysNum);
+     randCrysNum = Math.floor(Math.random() * 11) + 1;
+     console.log(randCrysNum);
 
-    var imageCrystal = $("<img>");
+    imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
     imageCrystal.attr("src", "assets/images/amethyst.jpg");
     imageCrystal.attr("data-crystalvalue", randCrysNum);
     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
     crystals.append(imageCrystal);
+
 }
+}
+
+startGame();
 
 //click event to capture values
 crystals.on("click", ".crystal-image", function () {
@@ -32,19 +39,32 @@ crystals.on("click", ".crystal-image", function () {
     var crystalValue = ($(this).attr("data-crystalvalue")); //pulls value of crystal
     crystalValue = parseInt(crystalValue); //converts string elemtn to interger
     counter += crystalValue;
-    // console.log(counter);
-    // console.log(crystalValue);
+    console.log(counter);
+    console.log(crystalValue);
 
     // All of the same game win-lose logic applies. So the rest remains unchanged.
     alert("New score: " + counter);
+      
 
     if (counter === randNum) {
+        console.log(counter);
+        console.log(randNum);
         alert("You win!");
         wins++;
+        alert("# of wins: " + wins);
+        // console.log(wins);
+        $("#wins").text(wins);
+
+        startGame();
     }
     else if (counter >= randNum) {
         alert("You lose!!");
-        losses++;
+         losses++;
+         alert("# of losses: " + losses);
+         // console.log(losses);
+        $("#losses").text(losses);
+
+     startGame();
     }
 
 });
